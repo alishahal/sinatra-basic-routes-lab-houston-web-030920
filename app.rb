9f9392ec5515daad17f70d96187d7c3b3e7730bd
Karim Macknojia <alishahal@msn.com>
2020-03-29 17:40:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,15 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    "My name is __" 
+    "My name is Karim Macknojia" 
   end
   
   get '/hometown' do
-    "My hometown is __"
+    "My hometown is Houston, TX"
     
   end
     
   get '/favorite-song'  do
-     
+     "My favorite song is Kal ho na ho"
   end
 end
